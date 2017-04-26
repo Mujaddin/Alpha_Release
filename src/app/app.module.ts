9c603286b981ec6c2319d4fc7e0cbb87d5cd9032
Mujaddin <muhammadjamaluddin2412@gmail.com>
2017-04-27 06:15:26 +0700

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MdSelectModule, MdCheckbox,MdListModule,MdButtonModule,MdInputModule,MdTabsModule} from '@angular/material';
+import{FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+
+import{BtnComponent}from './btn/btn.component';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule
   ],
   providers: [],
