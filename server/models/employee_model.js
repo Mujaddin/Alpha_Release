e27f730b265c01d482dbefe7f5daa85a58fe1273
trainee <trainee>
2017-04-28 15:19:21 +0700

    add, delete, update, insert, clear for backend

--- server/models/employee_model.js
+++ server/models/employee_model.js
@@ -13,13 +13,14 @@ var Employee = {
         return db.query("delete from t_employee where Id=?", [id], callback);
     },
     updateEmployee: function (id, Employee, callback) {
-        sqlquery=("UPDATE t_employee SET `name_first`="+Employee.name_first+", `mobile_no`="+Employee.mobile_no+", `name_last`="+Employee.name_last+", `div_main`="+Employee.div_main+", `status`="+Employee.status+", `gender`="+Employee.gender+", `date_birth`="+Employee.date_birth+", `nationality`="+Employee.nationality+", `grade`="+Employee.grade+", `marital`="+Employee.marital+", `div_sub`="+Employee.div_sub+", `img_employee`="+Employee.img_employee+", `email`="+Employee.email+" WHERE id=?",[id]);
+        sqlquery=("UPDATE t_employee SET `name_first`='"+Employee.name_first+"',`mobile_no`='"+Employee.mobile_no+"',`name_last`='"+Employee.name_last+"',`div_main`='"+Employee.div_main+"',`status`='"+Employee.status+"',`gender`='"+Employee.gender+"',`date_birth`='"+Employee.date_birth+"',`nationality`='"+Employee.nationality+"', `grade`='"+Employee.grade+"', `marital`='"+Employee.marital+"',`div_sub`='"+Employee.div_sub+"',`img_employee`='"+Employee.img_employee+"',`email`='"+Employee.email+"' WHERE id="+id+"");
+        console.log(sqlquery);
         return db.query(sqlquery,callback);
     }
     ,
     addEmployee: function (Employee, callback) {
         sqlquery = ("INSERT INTO t_employee (`name_first`, `mobile_no`, `name_last`, `div_main`, `status`, `gender`, `date_birth`, `nationality`, `grade`, `marital`, `div_sub`, `img_employee`, `email`) VALUES ('" + Employee.name_first + "','" + Employee.mobile_no + "','" + Employee.name_last + "','" + Employee.div_main + "','" + Employee.status + "','" + Employee.gender + "','" + Employee.date_birth + "','" + Employee.nationality + "','" + Employee.grade + "','" + Employee.marital + "','" + Employee.div_sub + "','" + Employee.img_employee + "','" + Employee.email + "')");
-        console.log(sqlquery);
+        
 
         return db.query(sqlquery, callback);
 
