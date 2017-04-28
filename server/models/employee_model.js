5517ccf4951e812b8ea7593efea90509ff2ea887
trainee <trainee>
2017-04-28 10:16:17 +0700

    dad

--- server/models/employee_model.js
+++ server/models/employee_model.js
@@ -21,17 +21,19 @@ var Employee ={
         var text="";//random text
         var possible ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         for (var i=0;i<5;i++) text +=possible.charAt(Math.floor(Math.random()*possible.length));
-        var base64d=Employee.img_employee.replace(/^data:image\/png;base64,/,"");
-        var path ="./public/images/"+text+dt.getDate()+dt.getMonth()+dt.getMilliseconds()+".png";
-        fs.writeFile(path, base64d,'base64',function(err){
-            if(err){
-                return console.log(err);
-            }
-            console.log("employee was added");
-        });
-        return db.query(`INSERT INTO t_employee(name_last, name_first, gender, date_birth, nationality, marital, mobile_no, div_sub, div_main, grade, email) 
-        VALUES 
-        (Employee.name_last,Employee.name_first,Employee.gender,Employee.date_birth,Employee.nation,Employee.marital,Employee.mobile_no,Employee.div_sub,Employee.div_main,Employee.grade,Employee.Email)`)
+        // var base64d=Employee.img_employee.replace(/^data:image\/png;base64,/,"");
+        // var path ="./public/images/"+text+dt.getDate()+dt.getMonth()+dt.getMilliseconds()+".png";
+        // fs.writeFile(path, base64d,'base64',function(err){
+        //     if(err){
+        //         return console.log(err);
+        //     }
+        //     console.log("employee was added");
+        // });
+return db.query("select * from t_employee",callback);
+
+        // return db.query(`INSERT INTO t_employee(name_last, name_first, gender, date_birth, nationality, marital, mobile_no, div_sub, div_main, grade, email) 
+        // VALUES 
+        // (Employee.name_last,Employee.name_first,Employee.gender,Employee.date_birth,Employee.nation,Employee.marital,Employee.mobile_no,Employee.div_sub,Employee.div_main,Employee.grade,Employee.Email)`)
     }
 }
 module.exports=Employee;
